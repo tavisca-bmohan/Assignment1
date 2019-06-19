85fc2d66378bfd9d8b3998344339cce26e7aa82a
Brij Mohan <bmohan@tavisca.in>
2019-06-19 20:07:24 +0530

    Made interactive

---
+++ lib-master/Hello.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int add(int a, int b){
+   return a+b;
+}
+
+int main(int argc, char* argv[]){
+   string s;   
+   if(argc > 0){
+      s = argv[1];
+   }else{
+     s = 'N';
+   }
+    
+   cout <<s<<endl;
+   cout<<"Hello WOrld of git";
+   cout<<endl<<add(34,26);
+   cout<<"\nlearning Git";
+   return 0;
+}
